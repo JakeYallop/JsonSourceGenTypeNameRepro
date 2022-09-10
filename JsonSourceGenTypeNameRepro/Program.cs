9fe56f351dea010731a162b60fc8096521b24a5b
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2022-09-10 11:51:37 +0100

    Add project files.

---
+++ JsonSourceGenTypeNameRepro/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+[JsonSerializable(typeof(IEnumerable<One.MyClass>), TypeInfoPropertyName = "IEnumerableOneMyClass")]
+//[JsonSerializable(typeof(One.MyClass), TypeInfoPropertyName = "OneMyClass")]
+[JsonSerializable(typeof(IEnumerable<Two.MyClass>))]
+public partial class JsonContext : JsonSerializerContext
+{
+
+}
+
+namespace One
+{
+    public class MyClass
+    {
+        public int Prop { get; set; }
+    }
+}
+
+namespace Two
+{
+    public class MyClass
+    {
+        public int Prop { get; set; }
+    }
+}
